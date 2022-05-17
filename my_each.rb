3f273ead27d5074d6255ead1e022458c24b41fc4
Matthew <mattj20@gmail.com>
2022-05-16 17:28:39 -0700

    commit tree 5b9bc890a9a7dd5b6c94f4a5d7eda6e0a7b1c1c8

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+require 'pry'
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < array.length
+      test1 = array[i]
+      yield(test1)
+      i += 1
+    end
+  end
+  array
 end
